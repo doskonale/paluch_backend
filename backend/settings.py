e25bdd31b7a1f9cf43d7858bd6a6212e33e98cf9
Wojciech Cieslak <spie.od.sciany@gmail.com>
2020-11-17 22:37:21 +0100

    * set prod

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'i#b(0t4g0id2mrz(-b&s$jmb9xabegrrzipjnys3v__@iyo1sd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
