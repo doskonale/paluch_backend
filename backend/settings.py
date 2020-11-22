1688f5ec266e89b2a8ce43c4252f1ea7760c7cb6
Wojciech Cieslak <spie.od.sciany@gmail.com>
2020-11-22 23:40:07 +0100

    * update rest default auth classes

--- backend/settings.py
+++ backend/settings.py
@@ -104,6 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': []
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
