4d0f4205bf6874407c65c422f2f051c8d6c8c6be
Wojciech Cieslak <spie.od.sciany@gmail.com>
2020-12-07 15:19:59 +0100

    * add default paggination

--- backend/settings.py
+++ backend/settings.py
@@ -106,6 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 15,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
