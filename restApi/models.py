ea9b0fcda5cb6c61fb5cd4657f9db2a797f81fa3
Wojciech Cieslak <spie.od.sciany@gmail.com>
2020-12-15 00:59:28 +0100

    * set side_nav default value to false

--- restApi/models.py
+++ restApi/models.py
@@ -26,6 +26,6 @@ class Post(models.Model):
     content = models.TextField(blank=False, null=True)
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
-    side_nav = models.BooleanField(default=True)
+    side_nav = models.BooleanField(default=False)
     def __str__(self):
         return self.title       
