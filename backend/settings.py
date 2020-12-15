6bbd35fc220cbd33d28ebe10b5fa3f05643fe4ef
Wojciech Cieslak <spie.od.sciany@gmail.com>
2020-12-15 01:06:53 +0100

    * update max upload memory size

--- backend/settings.py
+++ backend/settings.py
@@ -129,6 +129,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
