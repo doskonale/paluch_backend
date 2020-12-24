d4da44c3a28ea6f490a26f420e11c5719c7db403
Wojciech Cieslak <spie.od.sciany@gmail.com>
2020-12-24 14:20:42 +0100

    * update settings

--- backend/settings.py
+++ backend/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'paluch',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'rootroot',
         'HOST': 'localhost',
         'PORT': '',
     }
