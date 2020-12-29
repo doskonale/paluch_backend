6cbf9501f20ece0d11ff134e135b28471501e250
Wojciech Cieslak <spie.od.sciany@gmail.com>
2020-12-29 23:37:18 +0100

    * remove files on delete

--- restApi/models.py
+++ restApi/models.py
@@ -5,12 +5,18 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
 
-
+@receiver(post_delete, sender=MyModel)
+def submission_delete(sender, instance, **kwargs):
+    instance.file.delete(False) 
+    
 class File(models.Model):
     name = models.CharField(max_length=500,blank=True, null=True)
     module = models.CharField(max_length=500,blank=False, null=True)
@@ -18,7 +24,10 @@ class File(models.Model):
     file = models.FileField(blank=False, null=False, upload_to='media/')
     def __str__(self):
         return self.file.name        
-
+    def delete(self, using=None, keep_parents=False):
+        self.song.storage.delete(self.song.name)
+        self.image.storage.delete(self.song.name)
+        super().delete()
 
 class Post(models.Model):
     title = models.CharField(max_length=300,blank=False, null=True)
