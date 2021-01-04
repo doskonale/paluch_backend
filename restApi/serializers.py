44843cbe4c36fed6d397b6f1863160b0313e5a9d
Wojciech Cieslak <spie.od.sciany@gmail.com>
2021-01-04 20:53:09 +0100

    * update info model

--- restApi/serializers.py
+++ restApi/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from restApi.models import File, Post
+from restApi.models import File, Post, GardenInfo
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
